584b3a39b20dc80d37575164558d493c8cbd8936
Nívea Suzana <nivea.xavier@escola.pr.gov.br>
2025-08-07 12:14:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import json 
+
+ARQUIVO_CADASTROS = "cadastros.json"
+
+def exibir_menu():
+    print("\n========== MENU CADASTRO ==========")
+    print("1. Cadastrar pessoa")
+    print("2. Ver cadastros")
+    print("3. Sair")
+    print("=====================")
+
+def salvar_cadastros(cadastros):
+    with open (ARQUIVO_CADASTROS, "w", encoding="utf-8") as arquivo:
+        json.dump(cadastros, arquivo, indent=4, ensure_ascii=False)
+    
+def carregar_cadastros():
+    try:
+        with open(ARQUIVO_CADASTROS, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def cadastrar_pessoa(cadastros):
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    turma = input("Turma: ")
+    curso = input("Curso: ")
+
+    cadastros.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso})
+    salvar_cadastros(cadastros)
+    print("Cadastro realizado com sucesso!")
+
+def ver_cadastros(cadastros):
+    if not cadastros:
+        print("\nNenhum cadastro realizado.")
+    else:
+        print("n\====== LISTA DE CADASTROS ======")
+        for i, pessoa in enumerate(cadastros, 1):
+            print(f"{i}. Nome: {pessoa['Nome']}, Idade:{pessoa['idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
+            input("\nPessione Enter para voltar ao menu...")
+
+    
